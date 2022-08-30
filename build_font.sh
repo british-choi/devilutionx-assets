9e8d3e07098d8b2fbfd64f05f49cece5b3dba93b
Youngjun, Choi <british2@naver.com>
2022-08-30 14:31:34 +0900

    Remove cn, jp font

---
+++ build_font.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+declare -ra LANGS=(pl ru)
+
+main() {
+  set -x
+  build_fonts_mpq
+  du -sh *.mpq
+}
+
+build_fonts_mpq() {
+  if ! which pcx2clx; then
+    echo "Please install pcx2clx from https://github.com/diasurgical/devilutionx-graphics-tools/"
+    exit 1
+  fi
+  rm -f fonts.mpq
+  mkdir -p build/fonts
+  pcx2clx --output-dir build/fonts --transparent-color 1 --num-sprites 256 --quiet assets/fonts/*.pcx
+  cd build
+  find fonts -type f -exec smpq -M 1 -C BZIP2 -c ../fonts.mpq '{}' '+'
+  cd -
+}
+
+main "$@"
